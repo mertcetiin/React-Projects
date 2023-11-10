a8d72eaf3d0a7073ddbe5f6a65f7b3d89b4f625b
mertcetiin <mrtctn506@gmail.com>
2023-11-10 12:45:43 +0300

    loading delete and bug fixed

--- Tours/src/components/ToursPage.tsx
+++ Tours/src/components/ToursPage.tsx
@@ -6,19 +6,23 @@ function ToursPage() {
     const toursState = useToursStore((state) => state.toursState)
     const deleteBtn = useToursStore((state) => state.deleteBtn)
 
+
     return (
         <div>
             <h1>Our Tours</h1>
-            {toursState.length === 0 ? <DeletePage /> : toursState.map((item) => (
-                <div key={item.id}>
-                    <h3>{item.name}</h3>
-                    <h4>{item.price}</h4>
-                    <p>{item.info}</p>
+            {toursState.length === 0 ? (
+                <DeletePage />
+            ) : (
+                toursState.map((item) => (
+                    <div key={item.id}>
+                        <h3>{item.name}</h3>
+                        <h4>{item.price}</h4>
+                        <p>{item.info}</p>
 
-                    <button onClick={() => deleteBtn(item.id)}>Not Interested</button>
-                </div>
-            ))}
-
+                        <button onClick={() => deleteBtn(item.id)}>Not Interested</button>
+                    </div>
+                ))
+            )}
         </div>
     )
 }
--- Tours/src/states/store.tsx
+++ Tours/src/states/store.tsx
@@ -2,19 +2,19 @@ import { create } from "zustand";
 import { ToursInfo, ToursType } from "../components/ToursInfo";
 
 interface ToursState {
-    toursState: ToursType[]
-    deleteBtn: (id: number) => void
-    refleshBtn: () => void
+    toursState: ToursType[];
+    deleteBtn: (id: number) => void;
+    refleshBtn: () => void;
 }
 
-export const useToursStore = create<ToursState>()((set) => ({
+export const useToursStore = create<ToursState>((set) => ({
     toursState: ToursInfo,
 
     deleteBtn: (id) => set((state) => ({
-        toursState: state.toursState.filter((item) => item.id !== id)
+        toursState: state.toursState.filter((item) => item.id !== id),
     })),
 
     refleshBtn: () => set({ toursState: ToursInfo }),
 
 
-}))
+}));
