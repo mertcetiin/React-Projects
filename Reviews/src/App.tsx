e99c5ae6a5a183c4d232bee71986464b4e9888c8
mertcetiin <mrtctn506@gmail.com>
2023-11-13 14:47:23 +0300

    increase and index added

--- Reviews/src/App.tsx
+++ Reviews/src/App.tsx
@@ -2,18 +2,19 @@ import './App.css'
 import { useInfoStore } from './states/store';
 
 function App() {
-
-  const infoStates = useInfoStore((state) => state.infoStates)
+  const infoStates = useInfoStore((state) => state.infoStates);
+  const index = useInfoStore((state) => state.index)
+  const increase = useInfoStore((state) => state.increase)
 
   return (
     <div>
-      {infoStates.map((item) => (
-        <div key={item.id}>
-          <h3>{item.name}</h3>
-          <h4>{item.job}</h4>
-          <p>{item.info}</p>
-        </div>
-      ))}
+      <div key={infoStates[index].id}>
+        <h3>{infoStates[index].name}</h3>
+        <h4>{infoStates[index].job}</h4>
+        <p>{infoStates[index].info}</p>
+        <button>-</button>
+        <button onClick={increase}>+</button>
+      </div>
     </div>
   )
 }
--- Reviews/src/states/store.tsx
+++ Reviews/src/states/store.tsx
@@ -3,9 +3,22 @@ import { infoItems, InfoType } from "../components/InfoItems";
 
 interface ınfoItemState {
     infoStates: InfoType[];
+    index: number;
+    //  handleInfoStates: (index: number) => void;
+    increase: () => void;
 }
 
 
 export const useInfoStore = create<ınfoItemState>()((set) => ({
     infoStates: infoItems,
-}))
+    index: 0,
+
+    //   handleInfoStates: (index: number) => set((state) => ({ ...state, index, infoStates: state.infoStates })),
+
+    increase: () => set((state) => ({
+        index: (state.index + 1) % state.infoStates.length,
+        infoStates: state.infoStates,
+    })),
+
+
+}));
