cea18e06ed417f961dd7007642075350439d6132
mertcetiin <mrtctn506@gmail.com>
2023-11-15 12:04:17 +0300

    showInfo added

--- AccordionText/src/components/Home.tsx
+++ AccordionText/src/components/Home.tsx
@@ -1,19 +1,22 @@
 import { useInfoStore } from "../states/store"
+import { useState } from "react";
 
 function Home() {
 
     const dataStates = useInfoStore((state) => state.dataStates);
 
+    const [showInfo, setShowInfo] = useState(false);
+
     return (
         <div>
             {dataStates.map((item) => (
                 <div key={item.id}>
-                    <h3>{item.title}</h3>
-                    <p>{item.info}</p>
+                    <h3>{item.title} <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? '-' : '+'}</button></h3>
+                    <p>{showInfo ? item.info : ''}</p>
                 </div>
             ))}
         </div>
     )
 }
 
-export default Home
+export default Home;
--- AccordionText/src/states/store.tsx
+++ AccordionText/src/states/store.tsx
@@ -4,8 +4,13 @@ import { dataType, dataItems } from '../data/DataInfo';
 
 interface infoItemType {
     dataStates: dataType[];
+    displayBtn: () => void;
 }
 
 export const useInfoStore = create<infoItemType>()((set) => ({
     dataStates: dataItems,
+
+    displayBtn: () => set((state) => ({
+        dataStates: state.dataStates
+    })),
 }));
