069759cdb41ef169360bafb585f240b77b039d42
mertcetiin <mrtctn506@gmail.com>
2023-11-15 13:37:49 +0300

    zustand added and project finished

--- AccordionText/src/components/Home.tsx
+++ AccordionText/src/components/Home.tsx
@@ -1,21 +1,22 @@
 import { useInfoStore } from "../states/store"
-import { useState } from "react";
 
 function Home() {
 
     const dataStates = useInfoStore((state) => state.dataStates);
+    const showInfo = useInfoStore((state) => state.showInfo);
+    const toggleInfo = useInfoStore((state) => state.toggleInfo);
 
-    const [showInfo, setShowInfo] = useState(false);
 
     return (
         <div>
             {dataStates.map((item) => (
                 <div key={item.id}>
-                    <h3>{item.title} <button onClick={() => setShowInfo(!showInfo)}>{showInfo ? '-' : '+'}</button></h3>
-                    <p>{showInfo ? item.info : ''}</p>
+                    <h3>{item.title} <button onClick={() => toggleInfo(item.id.toString())}>{showInfo[item.id] ? '-' : '+'}</button></h3>
+                    <p>{showInfo[item.id] ? item.info : ''}</p>
                 </div>
-            ))}
-        </div>
+            ))
+            }
+        </div >
     )
 }
 
--- AccordionText/src/states/store.tsx
+++ AccordionText/src/states/store.tsx
@@ -5,12 +5,22 @@ import { dataType, dataItems } from '../data/DataInfo';
 interface infoItemType {
     dataStates: dataType[];
     displayBtn: () => void;
+    showInfo: { [key: string]: boolean };
+    toggleInfo: (itemId: string) => void;
 }
 
 export const useInfoStore = create<infoItemType>()((set) => ({
     dataStates: dataItems,
+    showInfo: {},
 
     displayBtn: () => set((state) => ({
         dataStates: state.dataStates
+    })),
+
+    toggleInfo: (itemId) => set((state) => ({
+        showInfo: {
+            ...state.showInfo,
+            [itemId]: !state.showInfo[itemId],
+        },
     })),
 }));
